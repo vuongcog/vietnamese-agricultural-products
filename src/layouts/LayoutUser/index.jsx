688e2edd9c425fcb03d5b439407beec8a6e83d4a
Huynh Nhat Vuong <huynhnhatvuong01012002@gmail.com>
2024-07-13 01:34:24 +0700

    test http

--- src/layouts/LayoutUser/index.jsx
+++ src/layouts/LayoutUser/index.jsx
@@ -3,8 +3,11 @@ import { Outlet } from 'react-router-dom';
 import UserHeader from '../../components/user/Header';
 import useInjectReducerSagaShopping from '../../useCustom/user/useInjectReducerSagaShopping';
 import PropTypes from '../../utils/prop-types';
+import axios from 'axios';
 const LayoutUser = ({ children }) => {
   useInjectReducerSagaShopping();
+
+  axios.get('https://vapi.vnappmob.com/api/province');
   return (
     <div>
       <UserHeader></UserHeader>
